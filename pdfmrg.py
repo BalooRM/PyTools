f309933cdd8d94f88e4c8766d22e2071ce8a4d40
BalooRM <richard_ballew@yahoo.com>
2019-07-21 16:36:57 -0400

    PDF merge utility. This application accepts file names as command line args and merges the files into a single PDF file. This version reflects an attempt to use compression and uses both the file merge and add as separate pages.

---
+++ pdfmrg.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+import sys
+import re
+import PyPDF2
+#from PyPDF2 import PdfFileMerger, PdfFileReader, PdfFileWriter
+
+appname = 'pdfmrg.py'
+usage = """  This script merges multiple PDF files into a single file
+
+Usage: 
+ > python """ + appname + """ FILE1.PDF FILE2.PDF FILE3.PDF
+
+Output: merged.pdf
+"""
+#input("Press Enter to continue...")
+
+myargs = sys.argv    # read command line args
+if len(myargs) < 2:  # if there are not enough args, print usage and exit
+    print("ERROR: not enough parameters.\n")
+    print(usage)
+    #input("Press Enter to continue...")
+    sys.exit()
+
+pdfs = []
+for infile in myargs[1:]:
+    print(infile)
+    if not re.search("\.PDF$", infile, re.IGNORECASE):
+        print("ERROR: Input", infile, "is not a PDF file.\n")
+        #input("Press Enter to continue...")
+        sys.exit(usage)
+    pdfs.append(infile)
+
+#sys.exit('Goodbye')
+
+print(pdfs)
+input("Press Enter to continue...")
+
+#sys.exit('Goodbye')
+
+merger = PyPDF2.PdfFileMerger()
+writer = PyPDF2.PdfFileWriter()
+
+for pdf in pdfs:
+    reader = PyPDF2.PdfFileReader(pdf)
+    for i in range(reader.numPages):
+        page = reader.getPage(i)
+        page.compressContentStreams()
+        writer.addPage(page)
+    merger.append(pdf)
+
+merger.write('Scan2merge.pdf')
+with open('Scan2PageWrite.pdf', 'wb') as f:
+    writer.write(f)
