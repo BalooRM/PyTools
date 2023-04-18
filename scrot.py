49529a4e6a6b33327d89df10e9aa502edd6f8582
rmbicfcom <richard.ballew@icf.com>
2023-04-18 17:04:45 -0400

    updated to return focus to window

--- scrot.py
+++ scrot.py
@@ -6,6 +6,23 @@ import datetime
 import pyautogui
 import sys
 
+# get app name to find its window
+app = sys.argv[0]
+if 'C:' in app:     # when invoked with #! (without python) grab from full path
+    app = sys.argv[0].split('\\')[-1]
+window = ''   # save window and position
+left = 0
+top = 0
+windows = pyautogui.getAllWindows()
+for w in windows:
+    wt = w.title
+#    if (('Windows' in wt) and (app in wt)):
+    if ((app in wt)):        
+        window = w
+        left = int(w.left)
+        top = int(w.top)
+        #print('left,top=', ','.join([str(left), str(top)]))
+
 timestamp = datetime.datetime.today().strftime("%Y%m%d_%H%M%S")
 print(timestamp)
 fname = 'screen' + timestamp + '.png'
@@ -14,3 +31,8 @@ screenshot.save(fname)
 
 im = Image.open(fname)
 im.show()
+print(fname)
+if window != '':     # if window found, click it to return focus
+    pyautogui.click(left+50, top+50)
+else:
+    print(app, 'window not found')
